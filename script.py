6394a118a93a24cd9ff77ed66a21fdc116db8873
noori09 <noorimehta09@gmail.com>
2026-05-20 01:45:18 +0530

    Day 1-2: Python basics and input/output

---
+++ script.py
@@ -0,0 +1,30 @@
+print("hello world")
+myint = 7
+print(myint)
+myfloat = 8.0
+print(myfloat)
+one = 1
+two = 2
+three = one + two
+print(three)
+hello = "hello"
+world = "world"
+print(hello + " " + world)
+
+a,b = 3,5
+print(a,b)
+
+# one = 1
+# two = "hello"
+# three = one + two
+# print(three)
+
+
+name = input("what is your name ")
+age = input("what is your age? ")
+print(f"Hi {name} your age is {age}")
+
+name = input("what is your name? ")
+birth_year = int(input("what is your birth year? "))
+age = 2026 - birth_year
+print(f"Hi {name}, you are {age} years old in 2026")
